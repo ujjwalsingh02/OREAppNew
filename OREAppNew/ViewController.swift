7d447a25669d54cbef3f714fbb0a7fe1b054cadd
Ujjwal <101248846+ujjwalsingh02@users.noreply.github.com>
2023-03-12 15:26:26 +0530

    Initial Commit

---
+++ OREAppNew/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OREAppNew
+//
+//  Created by Ujjwalsingh Rajput on 12/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
